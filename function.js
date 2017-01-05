ca0657a6c12470cfc990b8f0125c95f00a4135ea
TJ Wiegman <tjwieg@users.noreply.github.com>
2017-01-04 19:51:53 -0600

    Version 1.1 - Added zero/empty detection
    Added a section that checks to see if any inputs are zero (which also includes inputs being empty cells)

--- function.js
+++ function.js
@@ -1,4 +1,5 @@
-//Google sheets; calculate weighted harmonic mean; custom function; should be available in Sheets Add-ons
+//Google sheets; calculate weighted harmonic mean; custom function
+//Version 1.1, includes zero/empty detection
 /** Calculates the weighted harmonic mean.
 *
 * @param i The input array
@@ -8,24 +9,28 @@
 */
 
 function WHM(i, f) {
-  //checking that the inputs and weights are of equal length
-  if (f.length != i.length)
-    return "Ranges not equal length"
-    
-  else {
-    //declaring inital values for numerator and denominator
-    var top = 0;
-    var bottom = 0;
+  //checking that the inputs and weights are of equal length 
+       if (f.length != i.length) {
+    return "Ranges not equal length";
+       }
   
-    //sums all elements in f
-    for (var c = 0; c < f.length; c++)
-      top += parseInt(f[c]);
+  //checking if any inputs are zero
+  for (var c = 0; c < i.length; c++)
+    if (i[c] == 0 || f[c] == 0)
+      return "Ranges contain zero";
+  
+  //declaring inital values for numerator and denominator
+  var top = 0;
+  var bottom = 0;
+  
+  //sums all elements in f
+  for (var c = 0; c < f.length; c++)
+    top += parseInt(f[c]);
    
-    //sums f/i for each cell
-    for (var c = 0; c < i.length; c++)
-      bottom += (parseInt(f[c])/parseInt(i[c]));
-  
-    //returns numerator divided by denominator
-    return (top/bottom)
-      }
+  //sums f/i for each cell
+  for (var c = 0; c < i.length; c++)
+    bottom += (parseInt(f[c])/parseInt(i[c]));
+     
+  //returns numerator divided by denominator
+  return parseInt(top/bottom);
 }
